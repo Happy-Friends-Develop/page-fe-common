0bd75b2eb516637a074e7e7053529e1d0fd61c28
최민영 <128110770+Minch13r@users.noreply.github.com>
2025-11-21 21:45:26 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,15 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
-  base: '/astro-ecommerce'
+  site: 'https://creativetimofficial.github.io',  
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler', 
+          silenceDeprecations: ['legacy-js-api', 'mixed-decls', 'color-functions', 'global-builtin', 'import'],
+        },
+      },
+    },
+  },
 });
